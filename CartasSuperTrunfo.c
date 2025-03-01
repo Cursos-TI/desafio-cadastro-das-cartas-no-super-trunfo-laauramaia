a23e2668e38c0bebe7c9b92a5ad0476ff49fee8d
laauramaia <lauramaiasan@gmail.com>
2025-03-01 05:47:37 +0000

    Cartassupertrunfonovato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -31,8 +31,7 @@ printf("digite sua cidade: \n");
 scanf("%s", Cidade);
 printf("sua cidade e: %s\n", Cidade);
 
-printf("Digite a populacao: \n");
-scanf("%d", &populacao);
+printf("digite a populacao: \n");
 printf("a populacao e: %d\n", populacao);
 
 printf("digite sua area \n");
@@ -46,5 +45,48 @@ printf("O pib e: %f\n", pib);
 printf("digite o numero de pontos turisticos: \n");
 scanf("%d", &numeropontosturisticos);
 printf("Quantidade de pontos turisticos sao: %d\n", numeropontosturisticos);
+
+
+printf("Carta 02\n");
+   char Estado2[20] = "Saopaulo";
+   char Cidade2[20] = "Jandira";
+   char codigodacarta2[20] = "A04";
+   int populacao2 = 32500;
+   float areaemkm2 = 24.300;
+   float pib2 = 15.100;
+   int numeropontosturisticos2 = 10;
+
+   // Exibição das informações da carta 2
+
+   
+   printf("digite seu estado: \n");
+   scanf("%s", Estado2);
+   printf("o estado e: %s\n", Estado2);
+
+printf("Digite o seu codigo da carta: \n");
+scanf("%s", codigodacarta2);
+printf("o codigo da carta e: %s\n", codigodacarta2);
+
+printf("digite sua cidade: \n");
+scanf("%s", Cidade2);
+printf("sua cidade e: %s\n", Cidade2);
+
+printf("Digite a populacao: \n");
+scanf("%d", &populacao2);
+printf("a populacao e: %d\n", populacao2);
+
+printf("digite sua area \n");
+scanf("%f", &areaemkm2);
+printf("A area em km2 e: %f\n", areaemkm2);
+
+printf("digite o pib: \n");
+scanf("%f", &pib2);
+printf("O pib e: %f\n", pib2);
+
+printf("digite o numero de pontos turisticos: \n");
+scanf("%d", &numeropontosturisticos2);
+printf("Quantidade de pontos turisticos sao: %d\n", numeropontosturisticos2);
+
+return 0;
 
 }
