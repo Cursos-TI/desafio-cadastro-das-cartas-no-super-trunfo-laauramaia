ec3a85a5e51a5fbcbb5233bb62cf7a6665675567
laauramaia <lauramaiasan@gmail.com>
2025-03-01 01:45:25 +0000

    CartasSupertrunfo1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código iniciaL serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+/// @return 0;
 
-    return 0;
+int main () {
+
+   printf("Bem vindo ao super trunfo!\n");
+   printf("Carta 01\n");
+   char Estado[20] = "Riodejaneiro";
+   char Cidade[20] = "mangaratiba";
+   char codigodacarta[20] = "A03";
+   int populacao = 1210041;
+   float areaemkm = 150.300;
+   float pib = 20.200;
+   int numeropontosturisticos = 300;
+
+   // Exibição das informações da carta 1
+
+
+
+   printf("digite seu estado: \n");
+   scanf("%s", Estado);
+   printf("o estado e: %s\n", Estado);
+
+printf("Digite o seu codigo da carta: \n");
+scanf("%s", codigodacarta);
+printf("o codigo da carta e: %s\n", codigodacarta);
+
+printf("digite sua cidade: \n");
+scanf("%s", Cidade);
+printf("sua cidade e: %s\n", Cidade);
+
+printf("Digite a populacao: \n");
+scanf("%d", &populacao);
+printf("a populacao e: %d\n", populacao);
+
+printf("digite sua area \n");
+scanf("%f", &areaemkm);
+printf("A area em km2 e: %f\n", areaemkm);
+
+printf("digite o pib: \n");
+scanf("%f", &pib);
+printf("O pib e: %f\n", pib);
+
+printf("digite o numero de pontos turisticos: \n");
+scanf("%d", &numeropontosturisticos);
+printf("Quantidade de pontos turisticos sao: %d\n", numeropontosturisticos);
+
 }
